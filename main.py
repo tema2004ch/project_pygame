e112a2443badb89383d6b1b2a197e229c994effd
artemiy <cartem856@gmail.com>
2022-01-17 22:24:56 +0300

    осталось добавить федин код

--- main.py
+++ main.py
@@ -35,7 +35,9 @@ def menu():
     pygame.init()
     screen = pygame.display.set_mode(size)
     gameover = False
-    start_button = Button(200, 200, 150, 50, (255, 0, 0), 0, 'Arial', 40, '  start')
+    start_button = Button(200, 200, 300, 50, (203, 190, 181), 0, 'Arial', 40, '          Начало')
+    start_button1 = Button(200, 275, 300, 50, (203, 190, 181), 0, 'Arial', 40, ' Крестики-шарики')
+    exit_button = Button(200, 350, 300, 50, (203, 190, 181), 0, 'Arial', 40, '          Выход')
     while not gameover:
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -43,19 +45,36 @@ def menu():
                 if x >= start_button.x and x <= start_button.x + start_button.width and y >= start_button.y and y <= start_button.y + start_button.height:
                     gameover = True
                     puzzle()
+                elif x >= start_button1.x and x <= start_button1.x + start_button1.width and y >= start_button1.y and y <= start_button1.y + start_button1.height:
+                    #запуск шариков
+                    pass
+                elif x >= exit_button.x and x <= exit_button.x + exit_button.width and y >= exit_button.y and y <= exit_button.y + exit_button.height:
+                    exit()
             elif event.type == pygame.MOUSEMOTION:
                 x, y = event.pos
                 if x >= start_button.x and x <= start_button.x + start_button.width and y >= start_button.y and y <= start_button.y + start_button.height:
                     start_button.border = 5
                     start_button.process_draw(screen)
+                elif x >= start_button1.x and x <= start_button1.x + start_button1.width and y >= start_button1.y and y <= start_button1.y + start_button1.height:
+                    start_button1.border = 5
+                    start_button1.process_draw(screen)
+                elif x >= exit_button.x and x <= exit_button.x + exit_button.width and y >= exit_button.y and y <= exit_button.y + exit_button.height:
+                    exit_button.border = 5
+                    exit_button.process_draw(screen)
                 else:
                     start_button.border = 0
                     start_button.process_draw(screen)
+                    start_button1.border = 0
+                    start_button1.process_draw(screen)
+                    exit_button.border = 0
+                    exit_button.process_draw(screen)
             if event.type == pygame.QUIT:
                 gameover = True
             screen.fill(white)
             screen.blit(background, (0, 0))
             start_button.process_draw(screen)
+            start_button1.process_draw(screen)
+            exit_button.process_draw(screen)
             pygame.display.flip()
             pygame.time.wait(10)
     sys.exit()
@@ -93,7 +112,6 @@ def puzzle():
             dx, dy = 1, 0
         if key[pygame.K_SPACE]:
             Pause()
-            break
         #баг после отжатия паузы нет яблок
 
         x += dx * cell
@@ -128,9 +146,9 @@ def Pause():
     start = False
 
 
-    continue_button = Button(width - 400, 100, 276, 50, (255, 0, 0), 0, 'Calibri', 50, 'Продолжить')
-    restart_button = Button(width - 400, 175, 276, 50, (255, 0, 0), 0, 'Calibri', 50, 'Заново')
-    exit_button = Button(width - 400, 250, 276, 50, (255, 0, 0), 0, 'Calibri', 50, 'Выход')
+    continue_button = Button(width - 400, 100, 276, 50, (203, 190, 181), 0, 'Calibri', 50, 'Продолжить')
+    restart_button = Button(width - 400, 175, 276, 50, (203, 190, 181), 0, 'Calibri', 50, 'Заново')
+    exit_button = Button(width - 400, 250, 276, 50, (203, 190, 181), 0, 'Calibri', 50, 'Выход')
 
     screen.blit(background, (0, 0))
     continue_button.process_draw(screen)
@@ -150,19 +168,25 @@ def Pause():
                 x, y = event.pos
                 if x >= continue_button.x and x <= continue_button.x + continue_button.width and y >= continue_button.y and y <= continue_button.y + continue_button.height:
                     continue_button.border = 5
+                    continue_button.color = (255, 255, 255)
                     continue_button.process_draw(screen)
                 elif x >= restart_button.x and x <= restart_button.x + restart_button.width and y >= restart_button.y and y <= restart_button.y + restart_button.height:
                     restart_button.border = 5
+                    restart_button.color = (255, 255, 255)
                     restart_button.process_draw(screen)
                 elif x >= exit_button.x and x <= exit_button.x + exit_button.width and y >= exit_button.y and y <= exit_button.y + exit_button.height:
                     exit_button.border = 5
+                    exit_button.color = (255, 255, 255)
                     exit_button.process_draw(screen)
                 else:
                     continue_button.border = 0
+                    continue_button.color = (203,190,181)
                     continue_button.process_draw(screen)
                     restart_button.border = 0
+                    restart_button.color = (203,190,181)
                     restart_button.process_draw(screen)
                     exit_button.border = 0
+                    exit_button.color = (203,190,181)
                     exit_button.process_draw(screen)
 
 
